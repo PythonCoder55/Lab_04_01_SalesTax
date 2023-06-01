2ddb7ceea367d7f7095bc8ee80ce58130ac5b1a2
PythonCoder55 <edwardbarchv@gmail.com>
2023-06-01 12:31:55 -0400

    end of task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double purchasePrice = 10;
+        double salesTax = 0.05;
+        double totalCost;
+        double taxCost;
+
+        taxCost = purchasePrice * salesTax;
+        totalCost = purchasePrice + taxCost;
+
+        System.out.println("The computed sales tax cost for a purchase of " + purchasePrice + " With a sales tax of " + salesTax + " is " + taxCost);
+        System.out.println("The total price is " + totalCost);
     }
 }
